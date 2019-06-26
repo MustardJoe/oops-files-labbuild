2090ec20a1f38dfaab84d56246dcc86f7897487c
Jonathan Forney <jonathan.forney@gmail.com>
2019-06-26 11:15:56 -0700

    created file needs fixin

---
+++ rename-files.js
@@ -0,0 +1,21 @@
+const fs = require('fs');
+
+const readDirectory = (directory, callback) => {
+  fs.readdir(directory, (err, files) => {
+    callback(err, files);
+  });
+};
+
+const rename = (path, newPath, callback);
+
+
+const getModifiedTime = (path, callback) => {
+  fs.stat(path, (err, stats) => {
+    if(!stats) return callback(err);
+    callback(err, stats.mtime.toISOString());
+  });
+};
+
+module.exports = {
+  readDirectory,
+};
