a8b5a49af3edaffb733987fe1693cdbaa236cec0
Jonathan Forney <jonathan.forney@gmail.com>
2019-06-25 17:10:30 -0700

    have my file that creates files

--- file-maker.js
+++ file-maker.js
@@ -1,24 +1,22 @@
 const fs = require('fs');
 
-
-//here is my func that creates the files
+//here is my func that creates the files - i ran it in node instead of 
+//turning it into a proper function and calling it because I didn't
+//want it to be easey to use, since we are writing a function that
+//duplicates an accident (and creates 100 files)
 for(let i = 0; i < 100; i++) {
   console.log(i);
-  let data = i;
+  let data = '';
+
+  if(i < 15) {data = 'dinosaurs';}
+  else if(i < 30) {data = 'velociraptor';}
+  else if(i < 45) {data = 'HobGoblin';}
+  else if(i < 60) {data = 'forest bat';}
+  else if(i < 75) {data = 'wereBear';}
+  else if(i < 100) {data = 'Green Slime';}
+
   fs.writeFile((`./oops-files/${i}.txt`), data, (err) => {
     if(err) return console.error(err);
   });
   
 }
-
-// fs.writeFile(('message2.txt'), data, (err) => {
-//   if(err) return console.error(err);
-
-// });
-
-// template for func
-// const data = new Uint8Array(Buffer.from('Hello Node.js'));
-// fs.writeFile('message.txt', data, (err) => {
-//   if (err) throw err;
-//   console.log('The file has been saved!');
-// });
