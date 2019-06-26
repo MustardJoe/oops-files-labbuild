ee6e1ca85111c9db38fd0d5ddac384028183ab60
Jonathan Forney <jonathan.forney@gmail.com>
2019-06-26 08:51:37 -0700

    started writing a test

---
+++ index.test.js
@@ -0,0 +1,13 @@
+const fs = require('fs');
+
+describe('test suite for file oops thing', () => {
+  it('reads the contents of a file', () => {
+    fs.readFile('./oops-files/63.txt', { encoding: 'utf8' }, (err, data) => {
+      if(err) return console.error(err);
+      
+      console.log(data);
+    });
+    expect(data).toEqual('wereBat');
+
+  });
+});
