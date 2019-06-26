7747d75f5411277d458dbe503f58fedbd971fb55
Jonathan Forney <jonathan.forney@gmail.com>
2019-06-26 16:43:20 -0700

    created and fully developed create file file

---
+++ create-files.js
@@ -0,0 +1,36 @@
+const fs = require('fs');
+
+const animals = [
+  'dinosaurs',
+  'velociraptor',
+  'HobGoblin',
+  'forest bat',
+  'WereBear',
+  'Green Slime',
+  'Trogdor!',
+];
+
+const getAnimal = () => {
+  const index = Math.floor(Math.random() * animals.length);
+  return animals[index];
+};
+
+const createFiles = (directory, count, callback) => {
+  let writtenSoFar = 0;
+  for(let i = 0; i < count; i++) {
+
+    fs.writeFile(`${directory}/${i}.txt`, getAnimal(), err => {
+      if(err) return callback(err);
+      
+      writtenSoFar += 1;
+      if(writtenSoFar === count) callback();
+    });
+   
+  }
+};
+
+module.exports = {
+  getAnimal,
+  createFiles,
+};
+
