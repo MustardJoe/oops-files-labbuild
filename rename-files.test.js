f4ba6d94194f0e6f91e056936062811c8516b312
Jonathan Forney <jonathan.forney@gmail.com>
2019-06-27 22:53:51 -0700

    tests seem good

--- rename-files.test.js
+++ rename-files.test.js
@@ -1,33 +1,87 @@
 const fs = require('fs');
-import { createFiles } from './file-maker';
-import { readDirectory, renameEverything } from './rename-files';
+const { createFiles } = require('./create-files');
+const { 
+  readDirectory,
+  rename,
+  getModifiedTime,
+  readFile, 
+  renameEverything,
+} = require('./rename-files');
 
-describe('rename funcs', () => {
-  afterEach(done => {
-
+describe('rename and related funcs tests', () => {
+  beforeAll(done => {
+    fs.mkdir('./fixtures', done);
   });
-  it('gets all files in target dir', done => {
-    readDirectory('./fixtures', (err, files) => {
 
+  beforeEach(done => {
+    createFiles('./fixtures', 100, done);
+  });
+  
+  afterEach(done => {
+    fs.readdir('./fixtures', (err, files) => {
+      if(files.length === 0) done();
+      let deletedSoFar = 0;
+      files.forEach(file => {
+        fs.unlink(`./fixtures/${file}`, err => {
+          if(err) return done(err);
+          deletedSoFar += 1;
+          if(deletedSoFar === files.length) done();
+        });
+      });
     });
   });
 
-  it('gets modified time', () => {
+  it('gets all files in target dir', done => {
+    readDirectory('./fixtures', (err, files) => {
+      expect(files).toHaveLength(100);
+      done();
+    });
+  });
 
+  it('can rename a file given a path and new path', done => {
+    fs.readFile('./fixtures', { encoding: 'utf8' }, (err, oldFileContent) => {
+      rename('./fixtures/0.txt', './fixtures/new-name.txt', err => {
+        expect(err).toBeFalsy();
+
+        fs.readFile('./fixtures/new-name.txt', { encoding: 'utf8' }, (err, newFileContent) => {
+          expect(err).toBeFalsy();
+          expect(newFileContent).toEqual(oldFileContent);
+          done();
+        });
+      });
+    });
+  });
+
+  it('gets modified time for file', done => {
+    getModifiedTime('./fixtures/0.txt', (err, modifiedTime) => {
+      expect(err).toBeFalsy();
+      expect(modifiedTime).toEqual(expect.any(String));
+      done();
+
+    });
   });
 
   it('gets the contents of a file', done => {
     fs.readFile('./fixtures/0.txt', { encoding: 'utf8' }, (err, expectedContent) => {
-
+      readFile('./fixtures/0.txt', (err, resultContent) => {
+        expect(err).toBeFalsy();
+        expect(resultContent).toEqual(expectedContent);
+        done();
+      });
     });
   });
 
-  it('renames all files in a folder to content-f', done => {
+  it('renames all files in a folder to desired format', done => {
     renameEverything('./fixtures', err => {
       expect(err).toBeFalsy();
 
-      fs.readdir('./fixtures', (err, files))
-    })
-  })
-
+      fs.readdir('./fixtures', (err, files) => {
+        expect(files).toHaveLength(100);
+        files.forEach(file => {
+          expect(file).toMatch(/\w+-\d+-\d{4}-\d{2}-\d{2}T\d{2}:\d{2}:\d{2}\.\d+Z/);
+        });
+        done();
+      });
+    });
+  });
 });
